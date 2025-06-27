50689fb6ccd7c431732cb6217cd2e426148be1f5
Divya Mahajan <69398073+divu23@users.noreply.github.com>
2025-06-26 23:14:15 -0700

    Updated ChatInterface.tsx

--- src/components/ChatInterface.tsx
+++ src/components/ChatInterface.tsx
@@ -378,7 +378,7 @@ const ChatInterface: React.FC<ChatInterfaceProps> = ({ user, xp, onXPUpdate }) =
               </button>
             </div>
           </div>
-       </motion.div>  -- added 
+       </motion.div>  ----- added by divya
 
         {/* Right Sidebar - Financial Analysis */}
         <AnimatePresence>
